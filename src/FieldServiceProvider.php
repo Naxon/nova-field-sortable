fb42f95af94c84539d6b7ce27aac2863563931fc
Brian Dillingham <bdillingham88@gmail.com>
2018-12-26 23:04:18 -0500

    remove unused style sheet

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -20,7 +20,6 @@ class FieldServiceProvider extends ServiceProvider
     {
         Nova::serving(function (ServingNova $event) {
             Nova::script('nova-field-sortable', __DIR__.'/../dist/js/field.js');
-            Nova::style('nova-field-sortable', __DIR__.'/../dist/css/field.css');
         });
     }
 
